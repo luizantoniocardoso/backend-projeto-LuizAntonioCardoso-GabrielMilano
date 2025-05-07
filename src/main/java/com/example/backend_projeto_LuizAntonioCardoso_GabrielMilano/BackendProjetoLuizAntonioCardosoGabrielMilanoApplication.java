4aff92ebb0b5695d9fc2bd0d3ac5360fbda87eb8
luizantoniocardoso <la200.o.sc@gmail.com>
2025-05-07 17:00:28 -0300

    iniciando projeto

---
+++ src/main/java/com/example/backend_projeto_LuizAntonioCardoso_GabrielMilano/BackendProjetoLuizAntonioCardosoGabrielMilanoApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_projeto_LuizAntonioCardoso_GabrielMilano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendProjetoLuizAntonioCardosoGabrielMilanoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendProjetoLuizAntonioCardosoGabrielMilanoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backend_projeto_LuizAntonioCardoso_GabrielMilano/BackendProjetoLuizAntonioCardosoGabrielMilanoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_projeto_LuizAntonioCardoso_GabrielMilano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendProjetoLuizAntonioCardosoGabrielMilanoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
